e272a26bcd0c7d0180d155899f33252ea96c42c6
Aryan-Singh-Amethia <aryanamethia1998@gmail.com>
2023-09-19 02:13:38 +0530

    First Commit

---
+++ components/layout/layout.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './navbar'
+
+const Layout = (props) => {
+  return (
+    <div>
+       <Navbar/>
+       <main>
+        {props.children}
+       </main>
+    </div>
+  )
+}
+
+export default Layout
---
+++ components/layout/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header>
+        <nav>
+            <h1>Navbar</h1>
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
