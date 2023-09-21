860f1b5c62cb7248204a03e2d9d20b5d73643e12
Aryan-Singh-Amethia <aryanamethia1998@gmail.com>
2023-09-21 23:12:32 +0530

    Added Hamburger Drawer

---
+++ components/layout/drawer.js
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import classes from './drawer.module.css';
+import Image from 'next/image';
+
+const DRAWER_ANCHOR = 'left';
+
+const NavDrawer = () => {
+
+  const [state ,setState] = useState({left : false});
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <Box
+      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+    <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+    
+  return (
+        <React.Fragment>
+          <Button className={classes.navbar_hamburger}
+             onClick={toggleDrawer(DRAWER_ANCHOR, true)}>
+              <Image 
+                src={'/porfolio-hamburger-icon.png'}
+                width={50}
+                height={40}
+                alt='hamburger icon on Navbar'/>
+          </Button>
+          <Drawer
+            anchor={DRAWER_ANCHOR}
+            open={state[DRAWER_ANCHOR]}
+            onClose={toggleDrawer(DRAWER_ANCHOR, false)}
+          >
+            {list(DRAWER_ANCHOR)}
+          </Drawer>
+        </React.Fragment>
+        );
+}
+
+export default NavDrawer
--- components/layout/navbar.js
+++ components/layout/navbar.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import classes from './navbar.module.css'
+import Image from 'next/image'
+import Drawer from './drawer'
 
 const Navbar = () => {
   return (
-    <header>
-        <nav>
-            <h1>Navbar</h1>
+    <header className={classes.navbar_container}>
+        <nav className={classes.navbar_main}>
+            <div className={classes.navbar_drawer}>
+              {<Drawer/>}
+            </div>
+            <div className={classes.navbar_title}>
+               <h1>ARYAN SINGH AMETHIA</h1>
+            </div>
         </nav>
     </header>
   )
