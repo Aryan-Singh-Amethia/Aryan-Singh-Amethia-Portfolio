7ab2686791185db63bf995ce2f90f7351291ec3a
Aryan-Singh-Amethia <aryanamethia1998@gmail.com>
2023-10-04 00:52:06 +0530

    fixed text issue

--- components/skills/skill-wheel.js
+++ components/skills/skill-wheel.js
@@ -3,18 +3,31 @@ import styles from './skill-wheel.module.css';
 import Wheel from './wheel';
 
 const SkillWheel = () => {
+
+  return (
+    <div className={styles.skills}>
+      <WheelConatiner/>
+      <Text/>
+    </div>
+  );
+
+}
+
+const WheelConatiner = () =>{
   return ( 
     <div className={styles.container}>
        <Wheel/>
-       <Text/>
     </div>
   )
 }
 
 const Text = () => {
-    <section className={styles.skills}>
+  return(
+    <div className={styles.skills_text}>
          <h2>SKILLS</h2>
-    </section>
+    </div>);
 }
+
+
 
 export default SkillWheel;
---
+++ components/skills/skills.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styles from './skills.module.css';
+import Wheel from './wheel';
+
+const Skills = () =>{
+  <div className={styles.skills_container}>
+     {/* <SkillWheel/>
+     <Text/> */}
+  </div>
+}
+
+// const SkillWheel = () => {
+//   return ( 
+//     <div className={styles.skills_wheel}>
+//        <Wheel/>
+//     </div>
+//   )
+// }
+
+// const Text = () => {
+//     <section className={styles.skills_text}>
+//          <h2>SKILLS</h2>
+//     </section>
+// }
+
+export default Skills;
